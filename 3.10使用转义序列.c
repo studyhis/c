12c93c0b04ae0c43f0ec4996790e8ccdfa4ac89b
studyhis <88530243+studyhis@users.noreply.github.com>
2022-11-08 22:58:58 +0800

    第三章 数据和C

--- 3.10使用转义序列.c
+++ 3.10使用转义序列.c
@@ -4,10 +4,10 @@ int main()
 {
 	float salary;
 
-	printf("\aEnter your desired monthly salary: ");//  \a-¾¯±¨Éù
-	printf(" $_________\b\b\b\b\b\b\b");//  \b-ÍË¸ñ
+	printf("\aEnter your desired monthly salary: ");//  \a-è­¦æŠ¥å£°
+	printf(" $_________\b\b\b\b\b\b\b");//  \b-é€€æ ¼
 	scanf_s("%f", &salary);
-	printf("\n\t$%.2f a month is $%.2f a year.", salary,salary*12.0);//  \t-Ë®Æ½ÖÆ±í·û
+	printf("\n\t$%.2f a month is $%.2f a year.", salary,salary*12.0);//  \t-æ°´å¹³åˆ¶è¡¨ç¬¦
 
 	printf("\tGee!\n");
 
